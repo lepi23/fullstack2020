5eef6e414929db05646366f4508053b810b05a27
Leevi Saarinen <leevi.saarinen@tuni.fi>
2020-06-16 18:40:13 +0300

    3.8*: puhelinluettelon backend step8 + aiemmat

--- osa3/index.js
+++ osa3/index.js
@@ -1,7 +1,15 @@
+
 const express = require('express')
 const app = express()
 
-const persons=
+var morgan = require('morgan')
+//morgan.token('postData', (req, res) => { return req.body[''] })
+morgan.token('type', function (req, res) { return JSON.stringify(req.body)})
+app.use(express.json())
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :type'))
+
+
+var persons =
 [
     { 
       name: "Arto Hellas", 
@@ -24,10 +32,56 @@ const persons=
       id: 4
     }
 ]
+app.get('/info', (req, res) => {
+  res.send(
+    `<p>PhoneBook has info for ${persons.length} people</p>
+    <p>${new Date()}</p>`)
+})
+
 app.get('/api/persons', (req, res) => {
 res.json(persons)
 })
+
+app.get('/api/persons/:id', (req, res) => {
+  const id = Number(req.params.id)
+  const person = persons.find(person => person.id === id)
+  if (person)
+  {
+    res.json(person)
+  }
+  else
+  {
+    res.status(404).end()
+  }
   
+}) 
+app.delete('/api/persons/:id', (req, res) => {
+  const id = Number(req.params.id)
+  persons = persons.filter(person => person.id !== id)
+
+  res.status(204).end()
+})
+app.post('/api/persons', (req, res) => {
+  const body = req.body
+  if (!body.name) {
+    return res.status(400).json({ 
+      error: 'name missing' 
+    })
+  }
+  if (!body.number) {
+    return res.status(400).json({ 
+      error: 'number missing' 
+    })
+  }
+  const person = {
+    name : body.name,
+    number : body.number,
+    id : Math.floor(Math.random() * Math.floor(5000))
+  }
+  persons = persons.concat(person)
+  res.json(person)
+})
+
 const PORT = 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
